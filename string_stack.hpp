aa2999f2867a524385146da50e72892f8fb5c8ef
Dustin Van Tate Testa <toast27@gmail.com>
2016-09-01 11:04:45 -0400

    imported from https://gist.github.com/dvtate/4a49ec7c9bf5e9dfad5273f62b3d1801
    :)))

---
+++ string_stack.hpp
@@ -0,0 +1,64 @@
+#ifndef STRING_STACK_H
+#define STRING_STACK_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+
+class StrStack {
+public:
+
+	// how many times to double the number of lines
+	uint8_t sizeFactor;
+
+	char** buffer;
+  	size_t stackDepth;
+
+	char** stackHead;
+
+  	StrStack():
+  		sizeFactor(0),
+  		buffer((char**) malloc(256)),
+  		stackDepth(0),
+  		stackHead(buffer)
+  	{}
+
+  	~StrStack(){
+
+		for ( ; stackDepth > 0; stackDepth--)
+			free(*(buffer--));
+
+		free(buffer);
+
+	}
+
+	// resets the object
+	void clear();
+
+	// doubles the size of the buffer
+	void grow();
+
+	// pushes a line to the top of the stack
+	void push(const char* str);
+
+	// deletes the string at the top of the stack.
+	void pop();
+
+	// the string at the top of the stack
+	/// be sure to copy it before calling pop()
+	char* top()
+		{ return stackDepth ? *(buffer - 1) : NULL; }
+
+	size_t& size()
+		{ return stackDepth; }
+
+	void changeTop(const char* str);
+	void top(const char* str)
+		{ return changeTop(str);}
+
+};
+
+
+
+#endif
